c2a70fcdc1050d34fe14712207638393d4c37d28
Pavan Kumar <66913595+ascender1729@users.noreply.github.com>
2024-10-19 18:47:38 +0000

    Update htop view with safe local machine information

--- system_info_project/info/views.py
+++ system_info_project/info/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 import os
+import platform
 from datetime import datetime
+import psutil
 import pytz
 
 def htop(request):
@@ -9,10 +11,23 @@ def htop(request):
     ist = pytz.timezone('Asia/Kolkata')
     server_time = datetime.now(ist).strftime('%Y-%m-%d %H:%M:%S.%f')
     
+    # Safe system information
+    cpu_percent = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory()
+    disk = psutil.disk_usage('/')
+
     response = f"""
     <h1>System Information</h1>
     <p>Name: {name}</p>
-    <p>Username: {username}</p>
+    <p>User: {username}</p>
     <p>Server Time (IST): {server_time}</p>
+    <h2>System Details:</h2>
+    <ul>
+        <li>System: {platform.system()}</li>
+        <li>Release: {platform.release()}</li>
+        <li>CPU Usage: {cpu_percent}%</li>
+        <li>Memory: {memory.percent}% used</li>
+        <li>Disk: {disk.percent}% used</li>
+    </ul>
     """
     return HttpResponse(response)
--- system_info_project/system_info_project/urls.py
+++ system_info_project/system_info_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_htop(request):
+    return redirect('htop')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('info.urls')),
+    path('', redirect_to_htop),  # Add this line
 ]
